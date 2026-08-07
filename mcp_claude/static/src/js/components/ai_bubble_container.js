5b46d9071cca8f7d76c29d7bc9743883c89c7ddc
Deepak Gulani <dev.gulani@gmail.com>
2026-08-07 16:20:12 +0530

    Hardened AI Bubble circular FAB styling, event binding, and skeleton scoping

--- mcp_claude/static/src/js/components/ai_bubble_container.js
+++ mcp_claude/static/src/js/components/ai_bubble_container.js
@@ -1,33 +1,22 @@
-/** @odoo-module **/
-
 import { Component, useState } from "@odoo/owl";
-import { registry } from "@web/core/registry";
-import { AIBubbleTrigger } from "@mcp_claude/js/components/ai_bubble_trigger";
-import { AIChatWindow } from "@mcp_claude/js/components/ai_chat_window";
+import { AIBubbleTrigger } from "./ai_bubble_trigger";
+import { AIChatWindow } from "./ai_chat_window";
 
 export class AIBubbleContainer extends Component {
     static template = "mcp_claude.AIBubbleContainer";
     static components = { AIBubbleTrigger, AIChatWindow };
 
     setup() {
-        const savedState = localStorage.getItem("mcp_ai_window_open") === "true";
         this.state = useState({
-            isOpen: savedState,
+            isOpen: false,
         });
     }
 
     toggleWindow() {
         this.state.isOpen = !this.state.isOpen;
-        localStorage.setItem("mcp_ai_window_open", this.state.isOpen);
     }
 
     closeWindow() {
         this.state.isOpen = false;
-        localStorage.setItem("mcp_ai_window_open", false);
     }
 }
-
-// Register globally in Odoo 18 main_components registry
-registry.category("main_components").add("AIBubbleContainer", {
-    Component: AIBubbleContainer,
-});
--- mcp_claude/static/src/js/components/ai_bubble_trigger.js
+++ mcp_claude/static/src/js/components/ai_bubble_trigger.js
@@ -1,11 +1,9 @@
-/** @odoo-module **/
-
 import { Component } from "@odoo/owl";
 
 export class AIBubbleTrigger extends Component {
     static template = "mcp_claude.AIBubbleTrigger";
     static props = {
-        onClick: Function,
-        isOpen: Boolean,
+        isOpen: { type: Boolean },
+        onClick: { type: Function },
     };
 }
